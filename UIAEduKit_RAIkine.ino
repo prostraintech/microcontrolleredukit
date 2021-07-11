d9410d57e08a2a3c234361d8d6589564346a5653
ZharifZubaidi <zharif.z@prostrain.com.my>
2021-07-12 06:21:10 +0800

    Add another comment
    Heyyy

--- UIAEduKit_RAIkine.ino
+++ UIAEduKit_RAIkine.ino
@@ -3,6 +3,7 @@
 //Code for 2-Link Robotic Arm's inverse kinematics (ikine). Using X-Y coordinate to move robotic arm.
 //By Zharif Zubaidi (zharif.z@prostrain.com.my | ProStrain Technologies Sdn Bhd)
 //Hey there all!
+//Heyyyyy yooo
 
 // Define pin connections & motor's steps per revolution
 #define DIRPIN1 2
