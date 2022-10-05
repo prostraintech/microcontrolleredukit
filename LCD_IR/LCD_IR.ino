8ae2e8ec9f8af0307928ef3c041e4ea40f887160
prostraintechnologies <zharif96@gmail.com>
2022-10-06 06:29:09 +0800

    Update

--- LCD_IR/LCD_IR.ino
+++ LCD_IR/LCD_IR.ino
@@ -1,18 +1,13 @@
 /*
-LCD Demo Code
+LCD-IR Demo Code
 ProStrain Technologies
 By Zharif Zubaidi
 Please contact us at info@prostrain.com.my if you have any inquiries.
-
-  LiquidCrystal Library - Hello World
 
  Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
  library works with all LCD displays that are compatible with the
  Hitachi HD44780 driver. There are many of them out there, and you
  can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
 
   The circuit:
  * LCD RS pin to digital pin 12
@@ -21,28 +16,6 @@ Please contact us at info@prostrain.com.my if you have any inquiries.
  * LCD D5 pin to digital pin 4
  * LCD D6 pin to digital pin 3
  * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
 */
 
 // include the library code:
@@ -51,19 +24,38 @@ Please contact us at info@prostrain.com.my if you have any inquiries.
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int irPin = A5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
+  Serial.begin(9600);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.setCursor(0, 0);
+  lcd.print("IR Sensor Distance Reading (cm):");
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  //IR Reading
+  float volts = analogRead(irPin)*0.0048828125; //(5/1024)
+  int distance = 13*pow(volts,-1); //Volts^-1
+  delay(500);
+  if(distance <= 30){
+    lcd.clear();
+    delay(1);
+    //LCD Displaying Sensor reading
+    lcd.setCursor(0, 0);
+    lcd.print("IR Distance:");
+    lcd.setCursor(0, 1);
+    lcd.print(distance);
+  }
+  else{
+    lcd.clear();
+    delay(1);
+    lcd.setCursor(0, 0);
+    lcd.print("IR Distance:");
+    lcd.setCursor(0, 1);
+    lcd.print("Out of range");
+  }
 }
