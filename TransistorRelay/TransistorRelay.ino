de57001f47efdb40493fc094352d1f09311edcee
prostraintechnologies <zharif96@gmail.com>
2022-10-06 04:35:50 +0800

    Update

--- TransistorRelay/TransistorRelay.ino
+++ TransistorRelay/TransistorRelay.ino
@@ -1,35 +1,23 @@
+// Demo code for Transistor and Darlington Transistor tutorial
 // ProStrain Technologies
 // By Taufik Yunahar
 // Please contact us at info@prostrain.com.my if you have any inquiries.
 
-const int redLED = 13;
-const int blueLED = 12;
-const int optoIn1 = 4;
-const int optoIn2 = 3;
+const int proxSwitch = 2;
+const int baseTransistor = 7;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(redLED, OUTPUT);
-  pinMode(blueLED, OUTPUT);
-  //When using optocoupler and connect to Arduino digital input,
-  //need to use internal pullup.
-  pinMode(optoIn1, INPUT_PULLUP); // Input from 12V NC Emergency Stop
-  pinMode(optoIn2, INPUT_PULLUP); // Input from 24V NO Emergency Stop
+  pinMode(baseTransistor, OUTPUT);
+  pinMode(proxSwitch, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(optoIn1)==HIGH){
-    digitalWrite(redLED,HIGH);
+  if(digitalRead(proxSwitch)==HIGH){
+    digitalWrite(baseTransistor,HIGH);
   }
   else{
-    digitalWrite(redLED,LOW);
+    digitalWrite(baseTransistor,LOW);
   }
-  if(digitalRead(optoIn2)==HIGH){
-    digitalWrite(blueLED,HIGH);
-  }
-  else{
-    digitalWrite(blueLED,LOW);
-  }
-
 }
